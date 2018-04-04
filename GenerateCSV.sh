ae09597224b823f5826f8826e3d2f8c85014980b
Pradeep Tammali <pradeepkumartammali@gmail.com>
2018-04-05 02:15:44 +0530

    Thu Apr  5 02:15:44 IST 2018

--- GenerateCSV.sh
+++ GenerateCSV.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ $# -eq 2 ]; then
-    LIMIT=40000
+    LIMIT=40  # to generate 40KB file
     for((i=0;i<$2;i++))
     {
         FILE_NAME="$1$i.csv"
         echo "id,date,start time,end time,distance,amount,no of persons,longitude,latitude" >> "$FILE_NAME"
-        FILE_SIZE=$(du -k $FILE_NAME | cut -f 1)
-        while [ $FILE_SIZE -le $LIMIT ]
+        while [ $(du -k $FILE_NAME | cut -f 1) -le $LIMIT ]
         do
-            echo "`shuf -i 1-10 -n 1`,`date -d "2011-01-01 + $(shuf -i 1-2557 -n 1) days" +'%m-%d-%Y'`,00:00:00,00:00:00,`shuf -i 1-60 -n 1`,`shuf -i 100-1500 -n 1`,`shuf -i 1-6 -n 1`,`shuf -i 10-99 -n 1`.`shuf -i 100000-999999 -n 1`,`shuf -i 10-99 -n 1`.`shuf -i 100000-999999 -n 1`" >> "$FILE_NAME"
+            start_time=`date -d "$(date +%H:%M:%S) + $(shuf -i 0-24 -n 1) hours $(shuf -i 0-60 -n 1) minutes $(shuf -i 0-60 -n 1) seconds" +'%H:%M:%S'`
+            echo "`shuf -i 1-10 -n 1`,`date -d "2011-01-01 + $(shuf -i 1-2557 -n 1) days" +'%m-%d-%Y'`,$start_time,`date -d "$start_time + $(shuf -i 1-6 -n 1) hours $(shuf -i 0-60 -n 1) minutes $(shuf -i 0-60 -n 1) seconds" +'%H:%M:%S'`,`shuf -i 1-60 -n 1`,`shuf -i 100-1500 -n 1`,`shuf -i 1-6 -n 1`,`shuf -i 10-99 -n 1`.`shuf -i 100000-999999 -n 1`,`shuf -i 10-99 -n 1`.`shuf -i 100000-999999 -n 1`" >> "$FILE_NAME"
         done
     }
 else
