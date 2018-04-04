268e5cf9403f8049252b744e9d15a1c35f3b9dd5
Pradeep Tammali <pradeepkumartammali@gmail.com>
2018-04-05 02:24:34 +0530

    Thu Apr  5 02:24:34 IST 2018

--- GenerateCSV.sh
+++ GenerateCSV.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+### This script generates a test data files with extension .csv with following conditions
+# Id range 1 to 10
+# Date is between 2011 to 2018 in mm-dd-yyyy format
+# Start time in HH:MM:SS format
+# End time is in same format with some time added to start time
+# Distance range is 1 to 60
+# Persons 1 to 6
+# Amount is 100 to 1500
+# Longitude is with <two digits>.<six digits>
+# Latitude is same as Longitude
+
 if [ $# -eq 2 ]; then
     LIMIT=40  # to generate 40KB file
     for((i=0;i<$2;i++))
